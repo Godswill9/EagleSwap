7ca89fe8bf642cad91b61255dd4825c9b88eb16c
Godswill <ghycinth9@gmail.com>
2022-09-12 16:25:14 +0100

    new changes

---
+++ src/pages/Warning.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Warning() {
+  return (
+    <div className="notice">
+           <div className='inner'>
+              <h1>WARNING:</h1>
+              <span>please make sure you're visiting <a href='#'>https://exchange.babyswap.finance</a></span>   
+              <span>check the URL carefully</span>
+           </div>
+        </div>
+  )
+}
